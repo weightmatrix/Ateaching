d97e01fb6c2162ee4c58f838b687b1d44bb39114
weightmatrix <weightmatrix@163.com>
2026-04-19 23:04:20 +0800

    Initial Commit

---
+++ ATeaching/ATeachingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATeachingApp.swift
+//  ATeaching
+//
+//  Created by 李中正 on 2026/4/19.
+//
+
+import SwiftUI
+
+@main
+struct ATeachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ATeaching/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ATeaching
+//
+//  Created by 李中正 on 2026/4/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
